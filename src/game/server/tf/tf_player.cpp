22aa56d06529c106e4b3f6ff1b611f6ec8428190
KaisoBits <31374466+KaisoBits@users.noreply.github.com>
2025-08-03 15:54:40 +0200

    Bonk! Atomich Punch

--- src/game/server/tf/tf_player.cpp
+++ src/game/server/tf/tf_player.cpp
@@ -9343,7 +9343,7 @@ int CTFPlayer::OnTakeDamage( const CTakeDamageInfo &inputInfo )
 		if ( m_Shared.InCond( TF_COND_PHASE ) )
 		{
 			m_Shared.m_ConditionData[ TF_COND_PHASE ].m_nPreventedDamageFromCondition += info.GetDamage();
-			m_Shared.m_iPhaseDamage += info.GetDamage();
+			// m_Shared.m_iPhaseDamage += info.GetDamage();
 		}
 
 		bTookDamage = false;
